94100c1223c5b96dce3c3b0404d3c0b18319238b
Nicolas Ayllon <51245273+NicolasAyllon@users.noreply.github.com>
2022-10-25 17:18:40 +0000

    added Tree struct and Insert function

--- src/tree.go
+++ src/tree.go
@@ -1,7 +1,29 @@
 package main
 
+// Represents one node in a tree
 type Tree struct {
 	Value int
 	Left *Tree
 	Right *Tree
+}
+
+// Construct a new node with given value with no children (a leaf node)
+func newTree(val int) *Tree {
+	t := Tree{Value: val, Left: nil, Right: nil}
+	return &t
+}
+
+// Insert a value into a binary tree starting at node t
+func Insert(t *Tree, val int) *Tree {
+	if t == nil {
+		node := newTree(val)
+		return node
+	}
+	if val < t.Value {
+		t.Left = Insert(t.Left, val)
+	}
+	if val > t.Value {
+		t.Right = Insert(t.Right, val)
+	}
+	return t
 }
