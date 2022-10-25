dfebf84c50152ea0fee421548cc43c39c8c7382e
Nicolas Ayllon <51245273+NicolasAyllon@users.noreply.github.com>
2022-10-25 03:36:09 +0000

    added run_tests.py and command line flag parsing

---
+++ run_tests.py
@@ -0,0 +1,27 @@
+import subprocess
+
+INPUTS =  [ 
+            "input/simple.txt",
+          #  "input/coarse.txt",
+          #  "input/fine.txt"
+          ]
+
+HASH_WORKERS = [21]
+DATA_WORKERS = [78]
+COMP_WORKERS = [66]
+
+n_hash_workers = 21
+n_data_workers = 78
+n_comp_workers = 66
+
+ITERATIONS = 1
+
+for filename in INPUTS:
+    for i in range(ITERATIONS):
+        subprocess.call([
+          "./BST",
+          "-input={}".format(filename),
+          "-hash-workers={}".format(n_hash_workers) , 
+          "-data-workers={}".format(n_data_workers) ,
+          "-comp-workers={}".format(n_comp_workers) ,
+        ])
--- src/main.go
+++ src/main.go
@@ -4,22 +4,23 @@ import (
 	"fmt"
 	// "sync"
 	"os"
-	//"flag"
+	"flag"
 )
 
 func main() {
 
 	argsWithProg := os.Args
 	argsWithoutProg := os.Args[1:]
-	// -hash-workers= integer-valued number of threads
-	// -data-workers= integer-valued number of threads
-	// -comp-workers= integer-valued number of threads
-	// -input= string-valued path to an input file
-	arg := os.Args[3]
+
+	// Declare flags (pointers), default values, and then parse
+	n_hash_workers := flag.Int("hash-workers", 1, "goroutines for tree hashing")
+	n_data_workers := flag.Int("data-workers", 0, "goroutines for hash tracking")
+	n_comp_workers := flag.Int("comp-workers", 0, "goroutines for comparison")
+	input_file := flag.String("input", "", "input filename containing BSTs")
+	flag.Parse()
+
+	fmt.Printf("hash-workers = %v\ndata-workers = %v\ncomp-workers = %v\ninput = %v\n", *n_hash_workers, *n_data_workers, *n_comp_workers, *input_file)
 
 	fmt.Println(argsWithProg)
 	fmt.Println(argsWithoutProg)
-	fmt.Println(arg)
-
-  fmt.Println("hello world")
 }
