f2be558c6dc3c8555b2e7fc10f07ae99064e6a1f
Nicolas Ayllon <51245273+NicolasAyllon@users.noreply.github.com>
2022-11-08 03:21:09 +0000

    refactored compareTrees into function, but groups are not formed properly yet

--- src/BST.go
+++ src/BST.go
@@ -46,39 +46,9 @@ func main() {
 
 	// Compare possible duplicate trees with the same hash
 	// and put identical trees in Groups
-	allGroups := make([]Group, 0, len(trees))
-	i := 0 // starting group id, increment as groups are added
-	for _, ids := range mapHashToIds {
-		// Track groups for trees with this hash, possible all ids are unique
-		currentGroups := make([]Group, 0) // make([]Group, 0, len(*ids))
-		// // Put first id in a unique group
-		// // [?] refactor later so empty case is automatic?
-		// firstGroup := Group{i, []int{(*ids)[0]}}; i++
-		// currentGroups = append(currentGroups, firstGroup)
+	// TODO: add timing
+	uniqueGroups := compareTreesAndGroup(trees, mapHashToIds)
+	printUniqueGroups(uniqueGroups)
 
-		// For remaining ids with this hash
-		for _, id := range *ids {
-			// For each group in currentGroup
-			for _, group := range currentGroups {
-				// If this tree is equivalent to first in group (a Representative tree)
-				groupRepTree := trees[group.TreeIds[0]]
-				if trees[id].isEquivalentTo(groupRepTree) {
-					// Add its id to this group
-					group.add(id)
-					break
-				}
-			}
-			// This tree did not match any in currentGroups
-			// So, create a new group for it and add to currentGroups
-			newGroup := Group{i, []int{id}}
-			i++
-			currentGroups = append(currentGroups, newGroup)
-		}
-		// Append the groups for this hash to allGroups
-		allGroups = append(allGroups, currentGroups...)
-	}
-
-	for i, group := range allGroups {
-		fmt.Printf("group %v: %s", i, intsToString(group.TreeIds, " "))
-	}
+	
 }
--- src/group.go
+++ src/group.go
@@ -20,3 +20,36 @@ func (g *Group)add(id int) {
 }
 
 
+func compareTreesAndGroup(trees []*Tree, mapHashToIds map[int]*[]int) []Group {
+	allGroups := make([]Group, 0, len(trees))
+	i := 0 // starting group id, increment as groups are added
+	for _, ids := range mapHashToIds {
+		// Track groups for trees with this hash, possible all ids are unique
+		currentGroups := make([]Group, 0) // make([]Group, 0, len(*ids))
+		// // Put first id in a unique group
+		// // [?] refactor later so empty case is automatic?
+		// firstGroup := Group{i, []int{(*ids)[0]}}; i++
+		// currentGroups = append(currentGroups, firstGroup)
+
+		// For remaining ids with this hash
+		for _, id := range *ids {
+			// For each group in currentGroup
+			for _, group := range currentGroups {
+				// If this tree is equivalent to first in group (a Representative tree)
+				groupRepTree := trees[group.TreeIds[0]]
+				if trees[id].isEquivalentTo(groupRepTree) {
+					// Add its id to this group
+					group.add(id)
+					break
+				}
+			}
+			// This tree did not match any in currentGroups
+			// So, create a new group for it and add to currentGroups
+			newGroup := Group{i, []int{id}}; i++
+			currentGroups = append(currentGroups, newGroup)
+		}
+		// Append the groups for this hash to allGroups
+		allGroups = append(allGroups, currentGroups...)
+	}
+	return allGroups
+}
