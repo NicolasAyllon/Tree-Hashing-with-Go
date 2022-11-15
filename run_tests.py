78249c35ff417041408f0b69241f8e8b2d5db717
Nicolas Ayllon <51245273+NicolasAyllon@users.noreply.github.com>
2022-11-15 07:42:44 +0000

    edited test file

--- run_tests.py
+++ run_tests.py
@@ -6,45 +6,64 @@ import re
 
 INPUTS =  [ 
           #  "input/simple.txt"   #,
-            "input/coarse.txt"  #,
+          #  "input/coarse.txt"   ,
           #  "input/fine.txt"  
           ]
 # values of i
 HASH_WORKERS = [
                  1, 
-                #  2, 
-                #  4, 
-                #  8, 
-                #  16, 
-                # -1 # means N: the number of trees
+                 2, 
+                 4, 
+                 8, 
+                 16, 
+                -1 # means N: the number of trees
                ] 
 # values of j
 DATA_WORKERS = {
 #  i : possible values of j
 # hash-workers : [data-workers]
-    1: [1],
-  #   2: [1, 2],
-  #   4: [1, 4],
-  #   8: [1, 8],
-  #  16: [16],
-  #  -1: [1, -1]
+                1: [
+                    1
+                   ],
+                2: [
+                    1, 
+                    2
+                   ],
+                4: [
+                    1, 
+                    4
+                   ],
+                8: [
+                    1, 
+                    8
+                   ],
+               16: [
+                     1,
+                    16
+                   ],
+               -1: [
+                     1, 
+                    -1
+                   ]
 }
 COMP_WORKERS = [
                 1, 
-                # 2, 
-                # 4, 
-                # 8, 
-                # 16
+                2, 
+                4, 
+                8, 
+                16,
+                -1
                ]
-ITERATIONS = 20
+ITERATIONS = 1
 
-csvs = []
 for filename in INPUTS:
   for n_hash_workers in HASH_WORKERS:
     for n_data_workers in DATA_WORKERS[n_hash_workers]:
       for n_comp_workers in COMP_WORKERS:
+        csvs = []
         for i in range(ITERATIONS):
           csv = ["{}:{}/{}/{}".format(filename, n_hash_workers, n_data_workers, n_comp_workers)]
+          
             # subprocess.call([
             #   "./BST",
             #   "-input={}".format(filename),
@@ -53,7 +72,12 @@ for filename in INPUTS:
             #   "-comp-workers={}".format(n_comp_workers) ,
             # ])
 
+          # Only hashes (step 1)
+          # cmd = "./BST -input={} -hash-workers={}".format(filename, n_hash_workers)
+
+          # All steps: 1, 2, 3
           cmd = "./BST -input={} -hash-workers={} -data-workers={} -comp-workers={}".format(filename, n_hash_workers, n_data_workers, n_comp_workers)
+
           out = check_output(cmd, shell=True).decode("utf8")
 
           m_hash = re.search("hashTime = (.*)", out)
@@ -71,6 +95,6 @@ for filename in INPUTS:
       
           csvs.append(csv)
 
-    print("\n")
-    for csv in csvs:
-      print (", ".join(csv))
+        print("\n")
+        for csv in csvs:
+          print (", ".join(csv))
